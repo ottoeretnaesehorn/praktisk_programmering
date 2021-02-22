e767825f452e1c3a8875f0f8d606760707925ecc
Alberte <albertemork@hotmail.com>
2021-02-22 09:20:58 +0100

    Test of exercise 3 epsilon_C included (failed)

---
+++ exercises/exercise3_epsilon/epsilon_C/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "equal.h"
+
+int main()
+{
+	printf("Testing equal.h:\n");
+
+	double tau = 1.0e-10; 
+	double epsilon = 1.0e-10;
+
+	if (equal(1, 2, tau, epsilon) == 1)
+	{
+		printf("1 = 2 (failed)\n");
+	}
+	else 
+	{
+		printf("1 != 2 (passed)\n");
+	}
+
+	if (equal(1, 1, tau, epsilon) == 0)
+	{
+		printf("1 != 1 (failed)\n");
+	}
+	else
+	{
+		printf("1 = 1 (passed)\n");
+	}
+
+	return 0; 
+}
