557214b200d733f1d09d9becff3f601f7aac32c3
Alberte <albertemork@hotmail.com>
2021-03-18 13:47:14 +0100

    Exercise 6 finished

--- exercises/exercise6_plots/plots_C/complex_gamma_data_generator.c
+++ exercises/exercise6_plots/plots_C/complex_gamma_data_generator.c
@@ -6,11 +6,9 @@ complex complex_gamma(complex z)
 {
 	// Single precision gamma function (Gergo Nemes, from Wikipedia)
 
-	double PI = 3.14159265359; 
-	
 	if (creal(z) < 0) 
 	{
-		return PI/csin(PI*z)/complex_gamma(1 - z);
+		return M_PI/csin(M_PI*z)/complex_gamma(1 - z);
 	}
 	
 	if (creal(z) < 9)
@@ -18,7 +16,7 @@ complex complex_gamma(complex z)
 		return complex_gamma(z + 1)/z; 
 	}
 
-	complex ln_complex_gamma = z*clog(z + 1.0/(12.0*z - 1.0/z/10)) - z + clog(2*PI/z)/2;
+	complex ln_complex_gamma = z*clog(z + 1.0/(12.0*z - 1.0/z/10)) - z + clog(2*M_PI/z)/2;
 
 	return cexp(ln_complex_gamma);
 }
@@ -33,8 +31,10 @@ int main()
 	{
 		for (double y = min; y <= max; y += inc)
 		{
-			printf("%10g %10g %10g \n", x, y, cabs(complex_gamma(x + I*y)));
+			printf("%g \t %g \t %g \n", x, y, cabs(complex_gamma(x + I*y)));
 		}
+
+		printf("\n");
 	}
 
 	return 0;
